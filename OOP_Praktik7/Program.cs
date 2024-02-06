7b12eadd1baad8f73d17aeb9e03bd13ae94097a1
Марсель Баринов <barinov.marsel1111@gmail.com>
2024-02-06 23:15:06 +0300

    Добавьте файлы проекта.

---
+++ OOP_Praktik7/Program.cs
@@ -0,0 +1,13 @@
+﻿using Vector = OOP_Praktik7.Vector;
+
+Vector vector1 = new Vector(1, 2, 3);
+Vector vector2 = new Vector(4, 5, 6);
+vector1.Print();
+vector2.Print();
+Vector sum = vector1.Add(vector2);
+sum.Print();
+Vector difference = vector1.Subtract(vector2);
+difference.Print();
+Console.WriteLine("Length of vector1: " + vector1.Length);
+Console.WriteLine("Length of sum: " + sum.Length);
+Console.WriteLine("Length of difference: " + difference.Length);
---
+++ OOP_Praktik7/Vector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Praktik7
+{
+    public class Vector
+    {
+        public double x {  get; set; }
+        public double y { get; set; }
+        public double z{ get; set; }
+
+        Vector()
+        {
+            x = 0;
+            y = 0;
+            z = 0;              
+        }
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(x * x + y * y + z * z);
+            }
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("(" + x + ", " + y + ", " + z + ")");
+        }
+
+        public Vector Add(Vector otherVector)
+        {
+            return new Vector(x + otherVector.x, y + otherVector.y, z + otherVector.z);
+        }
+
+        public Vector Subtract(Vector otherVector)
+        {
+            return new Vector(x - otherVector.x, y - otherVector.y, z - otherVector.z);
+        }
+    }
+}
